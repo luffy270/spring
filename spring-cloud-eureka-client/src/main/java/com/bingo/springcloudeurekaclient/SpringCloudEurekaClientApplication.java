87cb6bf723ec95f053c9ad12676767e10d63ba49
luffy270 <41957771+luffy270@users.noreply.github.com>
2018-07-31 23:13:01 +0800

    new config

--- spring-cloud-eureka-client/src/main/java/com/bingo/springcloudeurekaclient/SpringCloudEurekaClientApplication.java
+++ spring-cloud-eureka-client/src/main/java/com/bingo/springcloudeurekaclient/SpringCloudEurekaClientApplication.java
@@ -1,10 +1,12 @@
 package com.bingo.springcloudeurekaclient;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -12,13 +14,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class SpringCloudEurekaClientApplication {
 
+	@Value("${test}")
+	private String test;
 	public static void main(String[] args) {
 		SpringApplication.run(SpringCloudEurekaClientApplication.class, args);
 	}
 
-	@RequestMapping("/status")
+	@RequestMapping(value = "/status",method = RequestMethod.GET)
 	public String rest()
 	{
-			return "status";
+			return test;
 	}
 }
